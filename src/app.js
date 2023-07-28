3e4f6ba01bff44cf6f315201acf68bdc573c4eb3
ankitgupta1999 <120298083+ankitgupta1999@users.noreply.github.com>
2023-07-28 12:02:28 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,8 +5,7 @@ const app = express();
 app.use(express.json());
 
 // Write the 3rd party RESPONSE-TIME middleware to get the desired result
-
-// GET endpointfor sending the result
+app.use(responseTime());
 app.get("/", function (req, res) {
   res.send("Checing response time");
 });
